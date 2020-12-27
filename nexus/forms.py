166d549d26fe713eac690c89fb5a7140b9aabf2c
Waldemar Peralta <waldeperalta1@gmail.com>
2020-12-27 04:35:42 -0300

    Finished "Create project" form page design

--- nexus/forms.py
+++ nexus/forms.py
@@ -2,6 +2,30 @@ from django import forms
 from .models import *
 
 class CreateProjectForm(forms.ModelForm):
+    title = forms.CharField(
+        label = 'Título: '
+    )
+
+    description = forms.CharField(
+        label = 'Descripción: ',
+        max_length = 150,
+        widget = forms.Textarea(
+            attrs = {
+                'rows': 3,
+            }
+        )
+    )
+    
+    image = forms.ImageField(
+        label = 'Imágen: ',
+        widget = forms.FileInput(
+            attrs = {
+                'class': 'custom-file-input',
+                'id': 'customFile'
+            }
+        )
+    )
+
     class Meta:
         model = Project
         fields = ['title', 'description', 'image']
